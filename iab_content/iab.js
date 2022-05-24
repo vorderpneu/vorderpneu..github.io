37b167ec3a4f497cb108dc38149aca12c487809f
p13646 <fabian.rust@css.ch>
2022-05-24 07:51:49 +0200

    first postMessage test impl

---
+++ iab_content/iab.js
@@ -0,0 +1,4 @@
+function postMessage(message){
+    if(!webkit.messageHandlers.cordova_iab) throw "Cordova IAB postMessage API not found!";
+    webkit.messageHandlers.cordova_iab.postMessage(JSON.stringify(message));
+}
