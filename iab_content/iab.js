4268377ad8ab1105867aa19ac00f18ff047bc77a
p13646 <fabian.rust@css.ch>
2022-05-24 12:04:05 +0200

    read message from input

--- iab_content/iab.js
+++ iab_content/iab.js
@@ -1,4 +1,7 @@
-function postMessage(message){
+function postMessage(){
+    const input = document.getElementById('messageInput');
     if(!webkit.messageHandlers.cordova_iab) throw "Cordova IAB postMessage API not found!";
-    webkit.messageHandlers.cordova_iab.postMessage(JSON.stringify(message));
+    webkit.messageHandlers.cordova_iab.postMessage(JSON.stringify({
+        message: input
+    }));
 }
