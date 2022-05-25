3769dde035bbf6d77a8dde462abc7ba06ed17898
p13646 <fabian.rust@css.ch>
2022-05-25 10:25:12 +0200

    add script to execute from ionic app

--- iab_content/send_message_to_iab.js
+++ iab_content/send_message_to_iab.js
@@ -1,4 +1,3 @@
-var message = 'this is the message';
-var messageObj = {message: message};
-var stringifiedMessageObj = JSON.stringify(messageObj);
-webkit.messageHandlers.cordova_iab.postMessage(stringifiedMessageObj);
+document.getElementById('executeScriptButton').addEventListener('click', postMessage());
+
+
