cba06eae646b89f53f174550c9793f0bbacc4838
p13646 <fabian.rust@css.ch>
2022-05-30 17:18:25 +0200

    param test

--- iab_content/script_with_param.js
+++ iab_content/script_with_param.js
@@ -1,5 +1,7 @@
 var scripts = document.getElementsByTagName('script');
 var jsFile = new URL("http://" + scripts[scripts.length-1].attributes.src.value);
 /*get value from query parameters*/
-console.log(`script with param timer: ${jsFile.searchParams.get("param")}`);
+
+var param = jsFile.searchParams.get("param");
+console.log(`script with param timer: ${param}`);
 document.getElementById('countdown_script_with_param').innerHTML = `<p>${param}</p>`;
