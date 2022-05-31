fd1d9ce5b86e788b07f6e9d19f4be8f0927a5547
p13646 <fabian.rust@css.ch>
2022-05-31 08:20:37 +0200

    data attribute test

--- iab_content/script_tag_with_data_attr.js
+++ iab_content/script_tag_with_data_attr.js
@@ -1,6 +1,6 @@
 var scripts = document.getElementsByTagName('script');
 var script = scripts[scripts.length-1]
 
-var param = script.getAtrribute('data-my_var');
+var param = script.getAttribute('data-my_var');
 console.log(`script with param timer: ${param}`);
 document.getElementById('countdown_script_with_param').innerHTML = `<p>${param}</p>`;
